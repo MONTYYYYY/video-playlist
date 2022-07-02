bd55d1d5d0c142a92a4d01d680e2ba0b80c99619
Monty Maikano <monty1134a@gmail.com>
2022-07-02 15:40:38 +0200

    Setup react-player + eslint

---
+++ src/components/VideoPlayer/VideoPlayer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+// Render a YouTube video player
+
+function VideoPlayer() {
+  return (
+    <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+  );
+}
+
+export default VideoPlayer;
